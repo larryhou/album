d8d53f4d7c3a2fb71de1ca339f6d05aa873fa254
doudou <doudou@doudou.local>
2018-06-24 10:44:59 +0800

    update

--- album_arrange.py
+++ album_arrange.py
@@ -2,54 +2,69 @@
 
 import argparse, os, sys, hashlib, re, time, json, tempfile
 
-DB_FIELD_NAME_HASH = 'hash'
-DB_FIELD_NAME_INDX = 'index'
+DATABASE_FIELD_NAME_INDEX = 'index'
+DATABASE_FIELD_NAME_HASH = 'hash'
+DATABASE_STORAGE_NAME = 'database.json'
 
-def main():
-    arguments = argparse.ArgumentParser()
-    arguments.add_argument('--import-path', '-i', required=True, help='local folder path for walking through to import')
-    arguments.add_argument('--work-path', '-w', default=os.path.expanduser('/Volumes/Remember/CameraRoll'), help='local folder path for moveing to')
-    arguments.add_argument('--hash-size', '-s', default=1024*10, help='num of bytes for md5sum caculation')
-    arguments.add_argument('--file-type', '-t', nargs='+', help='file extension types for keep-filter')
-    arguments.add_argument('--project', '-p', required=True, help='album project name')
-    data = arguments.parse_args(sys.argv[1:])
+class script_commands(object):
+    seperate_database = 'seperate-database'
+    import_project    = 'import-project'
+    import_assets     = 'import-assets'
 
+    @classmethod
+    def get_option_choices(cls):
+        choice_list = []
+        for name, value in vars(cls).items():
+            if name.replace('_', '-') == value: choice_list.append(value)
+        return choice_list
+
+class ArgumentOptions(object):
+    def __init__(self, data):
+        self.import_path = data.import_path # type: str
+        self.work_path = data.work_path # type: str
+        self.hash_size = data.hash_size # type: str
+        self.file_types = data.file_type # type: list
+        self.project_name = data.project_name # type: str
+        self.project_path = data.project_path # type: str
+        self.command = data.command # type: str
+
+def import_assets(options:ArgumentOptions):
     pattern = re.compile(r'\.(JPG|MOV|MP4)$', re.IGNORECASE)
-    if data.file_type:
-        type_list = data.file_type
-        pattern = re.compile(r'\.(%s)$'%('|'.join(type_list)), re.IGNORECASE)
+    if options.file_types:
+        pattern = re.compile(r'\.(%s)$' % ('|'.join(options.file_types)), re.IGNORECASE)
 
-    import_path = data.import_path
-    assert os.path.exists(import_path)
+    project_path = os.path.join(options.work_path, options.project_name)
+    if not os.path.exists(project_path):
+        os.makedirs(project_path)
 
-    proj_path = os.path.join(data.work_path, data.project)
-    if not os.path.exists(proj_path):
-        os.makedirs(proj_path)
+    database = {} # type: dict[str,dict]
+    def get_database(name:str, reload_from_disk = False)->dict:
+        if name in database and not reload_from_disk:
+            return database[name]
+        database_path = os.path.join(project_path, name)
+        data = {}
+        if os.path.exists(database_path):
+            try:
+                with open(database_path, 'r+') as fp:
+                    data = json.load(fp)
+            except _: pass
+        database[name] = data
+        return data
 
-    database = {}
-    database_location = os.path.join(proj_path, 'database.json')
-    if os.path.exists(database_location):
-        try:
-            with open(database_location, 'r+') as fp:
-                database = json.load(fp)
-        except: pass
-
-    for field_name in [DB_FIELD_NAME_HASH, DB_FIELD_NAME_INDX]:
+    for field_name in [DATABASE_FIELD_NAME_HASH, DATABASE_FIELD_NAME_INDEX]:
         if field_name not in database: database[field_name] = {}
 
-    hash_map = database.get(DB_FIELD_NAME_HASH)
-    indx_map = database.get(DB_FIELD_NAME_INDX)
-
     md5 = hashlib.md5()
-    hash_size = int(data.hash_size)
+    hash_size = int(options.hash_size)
     # generate incremental list
     increment_list = []
-    for walk_path,_, file_name_list in os.walk(import_path):
+    for walk_path, _, file_name_list in os.walk(options.import_path):
         for file_name in file_name_list:
             target_location = os.path.join(walk_path, file_name)
             if not pattern.search(file_name) or os.path.islink(target_location): continue
             timestamp = os.stat(target_location).st_birthtime
             mtime = time.localtime(os.path.getmtime(target_location))
+            hash_map = get_database(name=str(mtime.tm_year)).get(DATABASE_FIELD_NAME_HASH)
             with open(target_location, 'r+b') as fp:
                 md5.update(fp.read(hash_size))
                 digest = md5.hexdigest()
@@ -57,9 +72,11 @@ def main():
                 if digest in hash_map: continue
                 item = (timestamp, mtime, digest, target_location)
                 increment_list.append(item)
+
     def camera_roll_sort(a, b):
         if a[0] != b[0]: return 1 if a[0] > b[0] else -1
         return 1 if a[-1] > b[-1] else -1
+
     from functools import cmp_to_key
     increment_list.sort(key=cmp_to_key(camera_roll_sort))
     # generate image move path
@@ -69,33 +86,95 @@ def main():
     for n in range(len(increment_list)):
         _, mtime, digest, src_location = increment_list[n]
         label = '%02d%02d' % (mtime.tm_year, mtime.tm_mon)
-        if label not in indx_map: indx_map[label] = 1
+        index_map = get_database(name=str(mtime.tm_year)).get(DATABASE_FIELD_NAME_INDEX)
+        if label not in index_map: index_map[label] = 1
         common_path = src_location[:-4]
         sequence = live_map.get(common_path)
         if sequence is None:
-            sequence = indx_map.get(label)
+            sequence = index_map.get(label)
             live_map[common_path] = sequence
-            indx_map[label] += 1
+            index_map[label] += 1
         file_name = '%s_%04d%s' % (label, sequence, src_location[-4:])
-        dst_group_location = '%s/%04d'%(proj_path, mtime.tm_year)
+        dst_group_location = '%s/%04d' % (project_path, mtime.tm_year)
         if not os.path.exists(dst_group_location):
             os.makedirs(dst_group_location)
-        dst_location = '%s/%s'%(dst_group_location, file_name)
+        dst_location = '%s/%s' % (dst_group_location, file_name)
         assert not os.path.exists(dst_location)
         hash_map[digest] = file_name
-        bash_script.write('mv -v \'%s\' \'%s\'\n'%(src_location, dst_location))
-        print(digest, '%s => %s'%(src_location, dst_location))
-    bash_script.write('rm -f %s\n'%bash_script.name)
+        bash_script.write('mv -v \'%s\' \'%s\'\n' % (src_location, dst_location))
+        print(digest, '%s => %s' % (src_location, dst_location))
+    bash_script.write('rm -f %s\n' % bash_script.name)
     # bash_script.seek(0)
     # print bash_script.read()
     bash_script.close()
-    os.system('bash -e %s'%bash_script.name)
+    os.system('bash -e %s' % bash_script.name)
 
-    with open(database_location, 'w+') as fp:
-        json.dump(database, fp, indent=4)
+    for name, mini_database in database.items():
+        write_database(mini_database, project_path=os.path.join(project_path, name))
+
+def seperate_database(options:ArgumentOptions):
+    database = json.load(open('{}/{}'.format(options.project_path, DATABASE_STORAGE_NAME), 'r+'))
+    index_map = database.get(DATABASE_FIELD_NAME_INDEX) # type: dict
+    assert index_map
+    group_index_map = {} # type: dict[str:tuple[str, str]]
+    for name, value in index_map.items():
+        year = name[:4]
+        if year not in group_index_map: group_index_map[year] = []
+        group_index_map[year].append((name, value))
+    hash_map = database.get(DATABASE_FIELD_NAME_HASH) # type: dict
+    assert hash_map
+    group_hash_map = {} # type: dict[str:tuple[str, str]]
+    for hash, name in hash_map.items():
+        year = name[:4]
+        if year not in group_hash_map: group_hash_map[year] = []
+        group_hash_map[year].append((hash, name))
+    assert group_hash_map.keys() == group_index_map.keys()
+    for year in group_index_map.keys():
+        mini_project_path = os.path.join(options.project_path, year)
+        assert os.path.exists(mini_project_path)
+        mini_database = {}
+        index_map = mini_database[DATABASE_FIELD_NAME_INDEX] = {}
+        for key, value in group_index_map.get(year): index_map[key] = value
+        hash_map = mini_database[DATABASE_FIELD_NAME_HASH] = {}
+        for key, value in group_hash_map.get(year): hash_map[key] = value
+        write_database(mini_database, project_path=mini_project_path)
+
+def write_database(data:dict, project_path:str):
+    database_path = os.path.join(project_path, DATABASE_STORAGE_NAME)
+    with open(database_path, 'w+') as fp:
+        json.dump(data, fp, indent=4)
         fp.close()
+        print('database => {}'.format(database_path))
+
+def import_project(options:ArgumentOptions):
+    pass
 
 
+def main():
+    arguments = argparse.ArgumentParser()
+    arguments.add_argument('--command', '-c', default=script_commands.import_assets, choices=script_commands.get_option_choices())
+    arguments.add_argument('--import-path', '-i', help='local folder path for walking through to import')
+    arguments.add_argument('--work-path', '-w', default=os.path.expanduser('/Volumes/Remember/CameraRoll'), help='local folder path for moveing to')
+    arguments.add_argument('--hash-size', '-s', type=int, default=1024*10, help='num of bytes for md5sum caculation')
+    arguments.add_argument('--file-type', '-t', nargs='+', help='file extension types for keep-filter')
+    arguments.add_argument('--project-name', '-n', help='album project name')
+    arguments.add_argument('--project-path', '-p')
+    options = ArgumentOptions(data=arguments.parse_args(sys.argv[1:]))
+
+    if options.command == script_commands.import_assets:
+        assert options.import_path and os.path.exists(options.import_path)
+        assert options.file_types
+        assert options.work_path and os.path.exists(options.work_path)
+        assert options.hash_size >= 1024
+        assert options.project_name
+        import_assets(options)
+    elif options.command == script_commands.seperate_database:
+        assert options.project_path and os.path.exists(options.project_path)
+        seperate_database(options)
+    elif options.command == script_commands.import_project:
+        assert options.project_path and os.path.exists(options.project_path)
+        assert options.project_name
+        import_project(options)
 
 if __name__ == '__main__':
     main()
